a7143363f6c09754d26e7a7d4b7178ec157ee084
deficts <diegoar7@live.com>
2021-10-12 08:08:49 -0500

    Adding gitignore and compiler changes

--- compiler.py
+++ compiler.py
@@ -55,17 +55,21 @@ abstractTree = []
 def p_statement_declare_int(p):
     '''statement : INTDEC NAME is_assing
     '''
-    names[p[2]] = { "type": "INT", "value":0}
+    if type(p[3]) == 'float':
+        print('No puedes asignar flotantes a enteros')
+    else:
+        names[p[2]] = { "type": "INT", "value": p[3]}
 
 def p_is_assing(p):
     '''is_assing : "=" expression 
                 | '''
-    if 4 in p:
-        names[p[2]] = { "type": "INT", "value":p[4]}
+    p[0] = 0
+    if len(p) > 2:
+        p[0] = p[2]
 
 def p_statement_declare_float(p):
-    'statement : FLOATDEC NAME'
-    names[p[2]] = { "type": "FLOAT", "value":0}
+    'statement : FLOATDEC NAME is_assing'
+    names[p[2]] = { "type": "FLOAT", "value":p[3]}
 
 def p_statement_print(p):
     '''statement : PRINT '(' expression ')' '''
@@ -118,7 +122,7 @@ def p_expression_name(p):
 def p_error(p):
     if p:
         print(p)
-        print("Syntax error at line '%s' character '%s'" % (p.lexpos, p.lineno) )
+        print("Syntax error at line '%s' character '%s'" % (p.lineno, p.lexpos) )
     else:
         print("Syntax error at EOF")
 
